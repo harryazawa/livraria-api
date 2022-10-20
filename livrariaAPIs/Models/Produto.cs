5dd3cc6a98f397c8652fb0f15a2a768bfa027071
harry yazawa <harryazawa@gmail.com>
2022-10-20 17:29:57 -0300

    add: model for produto

---
+++ livrariaAPIs/Models/Produto.cs
@@ -0,0 +1,11 @@
+namespace livrariaAPIs.Models;
+
+public class Produto
+{
+    public string ID { get; set; }
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+    public int Quantidade { get; set; }
+    public string Categoria { get; set; }
+    public string Img { get; set; }
+}
