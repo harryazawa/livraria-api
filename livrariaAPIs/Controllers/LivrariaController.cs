4dacb4198ab29e734aa121d28262a7394cf2a8f1
harry yazawa <harryazawa@gmail.com>
2022-10-20 17:29:20 -0300

    add: db context and controller

---
+++ livrariaAPIs/Controllers/LivrariaController.cs
@@ -0,0 +1,52 @@
+using livrariaAPIs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace livrariaAPIs.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LivrariaController : ControllerBase
+{
+    private readonly ToDoContext _context;
+
+    public LivrariaController(ToDoContext context)
+    {
+        _context = context;
+        _context.ToDoProducts.Add(new Produto { ID = "1", Nome = "Alice no País das Maravilhas", Preco = 24.0, Quantidade = 1, Categoria = "Fantasia", Img = "img1"});
+        _context.ToDoProducts.Add(new Produto { ID = "2", Nome = "Senhor dos Anéis: As Duas Torres", Preco = 34.0, Quantidade = 1, Categoria = "Fantasia", Img = "img2"});
+        _context.ToDoProducts.Add(new Produto { ID = "3", Nome = "Sandman Vol. 1", Preco = 44.0, Quantidade = 1, Categoria = "Fantasia", Img = "img3"});
+        _context.ToDoProducts.Add(new Produto { ID = "4", Nome = "Blade Runner: Androides sonham com ovelhas elétricas?", Preco = 54.0, Quantidade = 1, Categoria = "Cyberpunk", Img = "img4"});
+        _context.ToDoProducts.Add(new Produto { ID = "5", Nome = "Fiction Lane", Preco = 64.0, Quantidade = 1, Categoria = "Fantasia", Img = "img5"});
+
+        _context.SaveChanges();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+    {
+        return await _context.ToDoProducts.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Produto>> GetItem(int id)
+    {
+        var item = await _context.ToDoProducts.FindAsync(id.ToString());
+
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        return item;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+    {
+        _context.ToDoProducts.Add(produto);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetItem), new { id = produto.ID }, produto);
+    }
+}
---
+++ livrariaAPIs/Models/ToDoContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace livrariaAPIs.Models;
+
+public class ToDoContext : DbContext
+{
+    public ToDoContext(DbContextOptions<ToDoContext> option) : base(option){}
+    public DbSet<Produto> ToDoProducts { get; set; }
+    
+}
